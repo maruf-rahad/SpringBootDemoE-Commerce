5bab092943937fcf64e96856a010909f5d296e10
Maruf Ahmed Rahad <marufrahad253@gmail.com>
2024-10-19 00:10:46 +0200

    added user signin mechanism and BcryptPasswordEncoder for password encyption

--- src/main/java/com/example/demomaven/controllers/UserController.java
+++ src/main/java/com/example/demomaven/controllers/UserController.java
@@ -1,4 +1,31 @@
 package com.example.demomaven.controllers;
 
+import com.example.demomaven.models.Users;
+import com.example.demomaven.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.cache.SpringCacheBasedUserCache;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/all-users")
+    public List<Users> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/register")
+    public Users register(@RequestBody Users user) {
+        return userService.registerUser(user);
+    }
 }
--- src/main/java/com/example/demomaven/services/UserService.java
+++ src/main/java/com/example/demomaven/services/UserService.java
@@ -1,4 +1,29 @@
 package com.example.demomaven.services;
 
+import com.example.demomaven.models.Users;
+import com.example.demomaven.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UsersRepository usersRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(10);
+
+
+    public List<Users> getAllUsers(){
+        return usersRepository.findAll();
+    }
+
+    public Users registerUser(Users user) {
+
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return usersRepository.save(user );
+    }
 }
